64285e9541737e835ddf6575339ac63f1f468677
Anael-Fer <166033786+Anael-Fer@users.noreply.github.com>
2024-04-17 06:14:16 -0300

    Add files via upload

---
+++ Lista1/Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cw + tab = abreviação do Console.WriteLine();
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Bom dia, viu.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lista1/Ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cw + tab = abreviação do Console.WriteLine();
+            Console.WriteLine("Olá mundo 2!");
+            Console.WriteLine("Boa tarde, viu.");
+            Console.ReadLine();
+        }
+    }
+}
